3f59ea01aa12d94709182e1c2ff87d6e4a453ad6
Billy Cropley <bcropley@hoshogroup.com>
2018-11-06 23:29:09 -0800

    Remove html

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
+import React, { Component, main } from "react";
 import ReactDOM from "react-dom";
 import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import icon from "./buttons.png";
 import "./App.css";
 
 export default class Login extends Component {
@@ -9,7 +10,7 @@ export default class Login extends Component {
 
     this.state = {
       email: "",
-      password: ""
+      password: "",
     };
   }
 
@@ -28,43 +29,44 @@ export default class Login extends Component {
   }
 
   render() {
-const element = (
-    <div>
-      <h1>Media!</h1>
-      <h2>Login!</h2>
-    </div>
-  );
-  // highlight-next-line
-  ReactDOM.render(element, document.getElementById('root'));
     return (
-      <div className="Login">
-        <form onSubmit={this.handleSubmit}>
-          <FormGroup controlId="email" bsSize="large">
-            <ControlLabel>Email</ControlLabel>
-            <FormControl
-              autoFocus
-              type="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-            />
-          </FormGroup>
-          <FormGroup controlId="password" bsSize="large">
-            <ControlLabel>Password</ControlLabel>
-            <FormControl
-              value={this.state.password}
-              onChange={this.handleChange}
-              type="password"
-            />
-          </FormGroup>
-          <Button
-            block
-            bsSize="large"
-            disabled={!this.validateForm()}
-            type="submit"
-          >
-            Login
-          </Button>
-        </form>
+      <div id="parent">
+        <div id="heading">
+          <h1>Billy and Austin's pay-to-use media server</h1>
+          <h3>Please login to continue</h3>
+        </div>
+        <div className="icon">
+          <img src={icon} width="200"/>
+        </div>
+        <div className="Login">
+          <form onSubmit={this.handleSubmit}>
+            <FormGroup controlId="email" bsSize="large">
+              <ControlLabel>Email</ControlLabel>
+              <FormControl
+                autoFocus
+                type="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+            <FormGroup controlId="password" bsSize="large">
+              <ControlLabel>Password</ControlLabel>
+              <FormControl
+                value={this.state.password}
+                onChange={this.handleChange}
+                type="password"
+              />
+            </FormGroup>
+            <Button
+              block
+              bsSize="large"
+              disabled={!this.validateForm()}
+              type="submit"
+            >
+              Send us btc
+            </Button>
+          </form>
+        </div>
       </div>
     );
   }
